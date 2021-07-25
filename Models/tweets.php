0bf2711be0faf6c0f45fa0e96ace2b691f32019c
oyama1128 <ten2808@icloud.com>
2021-07-25 18:25:12 +0900

    検索機能

--- Models/tweets.php
+++ Models/tweets.php
@@ -13,7 +13,7 @@ function createTweet(array $data)
 {
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // 接続チェック
-    if ($mysqli->connect_error) {
+    if ($mysqli->connect_errno) {
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
@@ -37,14 +37,15 @@ function createTweet(array $data)
  
     return $response;
 }
-
+ 
 /**
  * ツイート一覧を取得
  *
  * @param array $user ログインしているユーザー情報
+ * @param string $keyword 検索キーワード
  * @return array|false
  */
-function findTweets(array $user)
+function findTweets(array $user, string $keyword = null)
 {
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // 接続チェック
@@ -83,7 +84,20 @@ function findTweets(array $user)
         WHERE
             T.status = 'active'
     SQL;
-
+ 
+    // キーワードが入力されていた場合
+    if (isset($keyword)) {
+        // エスケープ
+        $keyword = $mysqli->real_escape_string($keyword);
+        // ツイート主のニックネーム・ユーザー名・本文から部分一致検索
+        $query .= ' AND CONCAT(U.nickname, U.name, T.body) LIKE "%' . $keyword . '%"';
+    }
+ 
+    // 新しい順に並び替え
+    $query .= ' ORDER BY T.created_at DESC';
+    // 50件表示
+    $query .= ' LIMIT 50';
+ 
     // SQL実行
     if ($result = $mysqli->query($query)) {
         // データを配列で受け取る
@@ -92,8 +106,8 @@ function findTweets(array $user)
         $response = false;
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
-
+ 
     $mysqli->close();
-
+ 
     return $response;
 }
